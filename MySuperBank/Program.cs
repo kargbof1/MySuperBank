e13cfcbbcad2de8928e3aaae9b13822699875e82
Fatmata Kargbo <fatmata.kargbo@ucdenver.edu>
2022-12-08 15:05:47 -0700

    second

--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -11,7 +11,32 @@ namespace MySuperBank
             account.MakeWithdrawal(120,DateTime.Now,"Hammock");
             Console.WriteLine(account.Balance);
 
+            //
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            //Test for negative balance:
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                return;
+            }
         }
     }
 }
 
+
+//https://www.youtube.com/watch?v=INfLHh3TDkk

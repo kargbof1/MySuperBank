8b790bdcb6cb6e4c40a850a211cd92b527a8a1cc
Fatmata Kargbo <fatmata.kargbo@ucdenver.edu>
2022-12-08 14:49:31 -0700

    first commit

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        //Properties:
+        //Bank Account Number
+        //Bank Account Owner
+        //Bank Account Balance
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+
+
+                }
+
+                return balance;
+            }
+        }
+        private static int accountNumberSeed = 123456789;
+        private List<Transaction> allTransactions=new List<Transaction>();
+
+        //Construct creates an individual 
+        //Construct a bank account
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance,DateTime.Now,"Initial Balance");
+            this.Number=accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+        }
+
+        //Methods and Functions:
+        //Deposits
+        //Withdrawals
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MySuperBank
+{
+   public  class program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Fifi",1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+            account.MakeWithdrawal(120,DateTime.Now,"Hammock");
+            Console.WriteLine(account.Balance);
+
+        }
+    }
+}
+
